fa04ecd0fc61fa99983f9b397dffad39990cab22
waltershaquil <81990995+waltershaquil@users.noreply.github.com>
2025-04-23 19:35:02 +0200

    home page tweaks

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,20 +4,20 @@ function Home() {
   return (
     <div className="bg-[#f3f3f3] w-full h-screen flex flex-col items-center justify-center">
       <p className="text-2xl font-bold">Welcome</p>
-      <div className="flex flex-row justify-center gap-4 bg-white rounded-xl shadow-xl py-6 px-4">
+      <div className="flex flex-col w-[400px] justify-center gap-4 bg-white rounded-xl shadow-xl py-6 px-4 items-center">
         <Link to="/CarReg">
-          <button className="bg-[#323232] text-white font-semibold w-40 rounded-lg p-2 hover:shadow-amber-200 hover:shadow-lg hover:bg-amber-400">
+          <button className="bg-[#323232] text-white font-semibold min-w-[350px] rounded-lg p-2 hover:shadow-amber-200 hover:shadow-lg hover:bg-amber-400">
             Cars
           </button>
         </Link>
 
         <Link to="/BuyerReg">
-          <button className="bg-[#323232] text-white font-semibold w-40 rounded-lg p-2 hover:shadow-amber-200 hover:shadow-lg hover:bg-amber-400">
+          <button className="bg-[#323232] text-white font-semibold min-w-[350px] rounded-lg p-2 hover:shadow-amber-200 hover:shadow-lg hover:bg-amber-400">
             Buyers
           </button>
         </Link>
         <Link to="/SaleReg">
-          <button className="bg-[#323232] text-white font-semibold w-40 rounded-lg p-2 hover:shadow-amber-200 hover:shadow-lg hover:bg-amber-400">
+          <button className="bg-[#323232] text-white font-semibold min-w-[350px] rounded-lg p-2 hover:shadow-amber-200 hover:shadow-lg hover:bg-amber-400">
             Sales
           </button>
         </Link>
